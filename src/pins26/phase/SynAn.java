3b087645d0b1889b516ad3d7c8bc9999348746bf
Matic Grobolsek <grobolsek.matic04@gmail.com>
2026-04-14 20:03:40 +0200

    finish syns

--- src/pins26/phase/SynAn.java
+++ src/pins26/phase/SynAn.java
@@ -11,6 +11,12 @@ public class SynAn implements AutoCloseable {
 
 	/** Leksikalni analizator. */
 	private final LexAn lexAn;
+  
+  private static final HashSet<Token.Symbol> CONST_SYMBOLS = new HashSet<>(List.of(Token.Symbol.INTCONST, Token.Symbol.STRINGCONST, Token.Symbol.CHARCONST));
+	private static final HashSet<Token.Symbol> BINARY_OPERATORS = new HashSet<>(List.of(Token.Symbol.MUL, Token.Symbol.DIV, Token.Symbol.MOD, Token.Symbol.ADD,
+			Token.Symbol.SUB, Token.Symbol.EQU, Token.Symbol.NEQ, Token.Symbol.LTH, Token.Symbol.GTH, Token.Symbol.LEQ, Token.Symbol.GEQ, Token.Symbol.AND, Token.Symbol.OR));
+  private Report.Locatable lastLoc = new Report.Location(0, 0);
+  private static final Set<Token.Symbol> DEFINITION_START_SYMBOLS = Set.of(Token.Symbol.FUN, Token.Symbol.VAR);
 
 	/**
 	 * Ustvari nov sintaksni analizator.
@@ -44,13 +50,74 @@ public class SynAn implements AutoCloseable {
 	 * Opravi sintaksno analizo.
 	 */
 	public void parse() {
-	        /*** TODO ***/
-                if (lexAn.peekToken().symbol() != Token.Symbol.EOF)
+    if (DEFINITION_START_SYMBOLS.contains(lexAn.peekToken().symbol())) {
+      parseDefinitions();
+    }	
+    if (lexAn.peekToken().symbol() != Token.Symbol.EOF)
 			Report.warning(lexAn.peekToken(),
 					"Unexpected text '" + lexAn.peekToken().lexeme() + "...' at the end of the program.");
 	}
 
-        /*** TODO ***/
+  private void parseDefinitions() {
+    while (DEFINITION_START_SYMBOLS.contains(lexAn.peekToken().symbol())) {
+      parseDefinition();
+    }
+  }
+
+  private void parseDefinition() {
+    switch (lexAn.peekToken().symbol()) {
+      case FUN -> parseFunction();
+      case VAR -> parseVariable();
+      default -> { return; }
+    }
+  }
+
+  private void parseVariable() {
+    Token varToken = lexAn.takeToken();
+    final Token name = check(Token.Symbol.IDENTIFIER);
+    final Token assignToken = check(Token.Symbol.ASSIGN);
+    parseInitializers();
+  }
+
+  private void parseInitializers() {
+    while (lexAn.peekToken().symbol() == Token.Symbol.COMMA) {
+      final Token initializer = lexAn.takeToken();
+      parseInitializer();
+    }
+  }
+
+  private void parseInitializer() {
+    switch (lexAn.peekToken().symbol()) {
+      case CHARCONST, STRINGCONST -> lexAn.takeToken();
+      case INTCONST -> {
+        lexAn.takeToken();
+        if (lexAn.peekToken().symbol() == Token.Symbol.MUL) {
+          lexAn.takeToken();
+          if (!CONST_SYMBOLS.contains(lexAn.peekToken().symbol())) {
+            Report.Error(lexAn.peekToken(), "Syntax error while parsing initializer '" + lexAn.peekToken().lexeme() + "'. Not a const!");
+          }
+          
+        }
+      }
+      default -> {
+        throw new Report.Error(lexAn.peekToken(), "Syntax error while parsing initializer '" + lexAn.peekToken().lexeme() + "'. Value expected.");
+			}
+    }
+  }
+
+  private void parseFunction() {
+    
+  }
+
+  private void parseParameters() {}
+
+  private void parseStatments() {}
+
+  private void parseStatment() {}
+
+  
+  
+
 
 	// --- ZAGON ---
 
